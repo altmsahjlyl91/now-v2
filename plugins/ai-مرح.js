9bd904845cbfd55598feaf2c89f88ff816b91aea
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-11 01:00:14 +0300

    Update ai-مرح.js

--- plugins/ai-مرح.js
+++ plugins/ai-مرح.js
@@ -1,39 +1,26 @@
-import translate from '@vitalets/google-translate-api';
-import axios from 'axios';
-import fetch from 'node-fetch';
 
-const handler = async (m, {conn, text, command, args, usedPrefix}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
-  const tradutor = _translate.plugins.fun_simi
-  
-  if (!text) throw `${tradutor.texto1} ${usedPrefix + command} Hola Bot*`;
-  try {
-      const resSimi = await simitalk(text);
-      conn.sendMessage(m.chat, { text: resSimi.resultado.simsimi }, { quoted: m });
-  } catch {
-      throw tradutor.texto2;
-  }
-};
-handler.help = ['simi', 'bot'].map((v) => v + ' <teks>');
-handler.tags = ['fun'];
-handler.command = /^((sim)?simi|bot|alexa|cortana)$/i;
-export default handler;
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-async function simitalk(ask, apikeyyy = "iJ6FxuA9vxlvz5cKQCt3", language = "es") {
-    if (!ask) return { status: false, resultado: { msg: "Debes ingresar un texto para hablar con simsimi." }};
-    try {
-        const response1 = await axios.get(`https://delirios-api-delta.vercel.app/tools/simi?text=${encodeURIComponent(ask)}`);
-        const trad1 = await translate(`${response1.data.data.message}`, {to: language, autoCorrect: true});
-        if (trad1.text == 'indefinida' || response1 == '' || !response1.data) trad1 = XD // Se usa "XD" para causar error y usar otra opción.  
-        return { status: true, resultado: { simsimi: trad1.text }};        
-    } catch {
-        try {
-            const response2 = await axios.get(`https://anbusec.xyz/api/v1/simitalk?apikey=${apikeyyy}&ask=${ask}&lc=${language}`);
-            return { status: true, resultado: { simsimi: response2.data.message }};       
-        } catch (error2) {
-            return { status: false, resultado: { msg: "Todas las API's fallarón. Inténtalo de nuevo más tarde.", error: error2.message }};
-        }
-    }
+ let lang = global.db.data.users[m.sender].language
+  if (!text) throw `✳️ ${mssg.notext}`
+  m.react('🗣️') 
+  try { 
+  //let res = await fetch(`https://api.simsimi.vn/v2/?text=${text}&lc=${lang}`)
+  let res = await fetch('https://api.simsimi.vn/v1/simtalk', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: `text=${encodeURIComponent(text)}&lc=${lang}&key=`
+  })
+  let json = await res.json()
+  m.reply(json.message.replace('simsimi', `${botName}`).replace('Simsimi', `${botName}`).replace('sim simi', `${botName}`))
+} catch {
+  m.reply(`❎ Intenta de nuevo mas tarde La api de SimSimi se cayo`)
 }
+
+}
+handler.help = ['bot']
+handler.tags = ['fun']
+handler.command = ['bot', 'simi'] 
+
+export default handler
