cdccd5d209320f2cc2c4619ebc820abf6f50bd9e
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-11 02:34:09 +0300

    Update ai-مرح.js

--- plugins/ai-مرح.js
+++ plugins/ai-مرح.js
@@ -1,26 +1,28 @@
+import fetch from 'node-fetch';
 
-import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
+  let lang = global.db.data.users[m.sender].language;
+  if (!text) throw `✳️ ${mssg.notext}`;
 
- let lang = global.db.data.users[m.sender].language
-  if (!text) throw `✳️ ${mssg.notext}`
-  m.react('🗣️') 
-  try { 
-  //let res = await fetch(`https://api.simsimi.vn/v2/?text=${text}&lc=${lang}`)
-  let res = await fetch('https://api.simsimi.vn/v1/simtalk', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: `text=${encodeURIComponent(text)}&lc=${lang}&key=`
-  })
-  let json = await res.json()
-  m.reply(json.message.replace('simsimi', `${botName}`).replace('Simsimi', `${botName}`).replace('sim simi', `${botName}`))
-} catch {
-  m.reply(`❎ Intenta de nuevo mas tarde La api de SimSimi se cayo`)
-}
+  // Remove the m.react line, as it's not supported in the current Baileys version
+  // m.react('🗣️'); 
 
-}
-handler.help = ['bot']
-handler.tags = ['fun']
-handler.command = ['bot', 'simi'] 
+  try {
+    //let res = await fetch(`https://api.simsimi.vn/v2/?text=${text}&lc=${lang}`);
+    let res = await fetch('https://api.simsimi.vn/v1/simtalk', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: `text=${encodeURIComponent(text)}&lc=${lang}&key=`
+    });
+    let json = await res.json();
+    m.reply(json.message.replace('simsimi', `${botName}`).replace('Simsimi', `${botName}`).replace('sim simi', `${botName}`));
+  } catch (error) {
+    m.reply(`❎ Intenta de nuevo mas tarde La api de SimSimi se cayo`);
+  }
+};
 
-export default handler
+handler.help = ['bot'];
+handler.tags = ['fun'];
+handler.command = ['bot', 'simi'];
+
+export default handler;
