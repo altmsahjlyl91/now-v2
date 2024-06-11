8882749ef0dc51fb63b05212ded4185ba21d4c93
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-11 18:35:47 +0300

    Update ai-مرح.js

--- plugins/ai-مرح.js
+++ plugins/ai-مرح.js
@@ -1,30 +1,36 @@
-import fetch from 'node-fetch';
-
-// مباشرة نستخدم المفتاح اللي زودتنا فيه، بس لا تنسى تحفظه بأمان بعدين
-const YOUR_API_KEY = 'vGLVun.sIkBdzyy13O3zv~Say0mDJFyYsXeaCykA';
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  let lang = global.db.data.users[m.sender].language;
-  if (!text) throw `✳️ ما في نص مدخل. جرب تدخل نص يا ياحوبي!`;
-
-  try {
-    let res = await fetch('https://api.simsimi.vn/v1/simtalk', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: `text=${encodeURIComponent(text)}&lc=${lang}&key=${YOUR_API_KEY}`
-    });
-
-    if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
-
-    let json = await res.json();
-    await m.reply(json.message.replace(/simsimi/gi, `${botName}`));
-  } catch (error) {
-    await m.reply(`❎ حاول مرة ثانية لاحقًا، شكله السيرفر واقع.`);
+  import translate from '@vitalets/google-translate-api'
+  import fetch from "node-fetch"
+  let handler = async (m, { text, command, args, usedPrefix }) => {
+    if (!text) throw `اكتب نصا للتحدث معي\nمثال: ${usedPrefix + command} مرحبا*`
+    try {
+    await conn.sendPresenceUpdate('composing', m.chat)
+    let api = await fetch("https://api.simsimi.net/v2/?text=" + text + "&lc=ar")
+    let resSimi = await api.json()
+    m.reply(resSimi.success)      
+    } catch {
+    try {
+    if (text.includes('مرحبا')) text = text.replace('مرحبا', 'مرحبا')
+    if (text.includes('مرحبا')) text = text.replace('مرحبا', 'اهلا')
+    if (text.includes('مرحبا')) text = text.replace('مرحبا', 'اهلا')    
+    let reis = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=" + text)
+    let resu = await reis.json()  
+    let nama = m.pushName || '1'
+    let api = await fetch("http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=" + nama + "&msg=" + resu[0][0][0])
+    let res = await api.json()
+    let reis2 = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=" + res.cnt)
+    let resu2 = await reis2.json()
+    m.reply(resu2[0][0][0])      
+    } catch {  
+    let reisss = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=id&dt=t&q=" + text)
+    let resuuu = await reisss.json()      
+    let res222 = await fetch(`https://violetics.pw/api/utility/simsimi?apikey=beta&text=${resuuu[0][0][0]}`)  
+    let json222 = await res222.json()
+    let resulttt = json222.result
+    let lolll = await translate(`${resulttt}`, { to: 'es', autoCorrect: true })
+    m.reply(lolll.text)      
+    }}
   }
-};
-
-handler.help = ['bot'];
-handler.tags = ['fun'];
-handler.command = ['bot', 'simi'];
-
-export default handler;
+  handler.help = ['sim simi']
+  handler.tags = ['General']
+  handler.command = ['bot', 'زورو', 'simi'] 
+  export default handler
