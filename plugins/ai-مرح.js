e070994ac5dfb1b12cba58ba5e547f1e3ffa1cba
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-11 20:28:10 +0300

    Update ai-مرح.js

--- plugins/ai-مرح.js
+++ plugins/ai-مرح.js
@@ -35,6 +35,6 @@ let handler = async (m, { text, command, args, usedPrefix }) => {
 
 handler.help = ['sim simi']
 handler.tags = ['General']
-handler.command = /^(.)$/i;
+handler.command = /^(\.)$/i;
 
 export default handler
