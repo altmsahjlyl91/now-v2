30a5df3ce07015d276a7864b9121a898c6d788f3
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-12 02:49:32 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -86,14 +86,7 @@ global.flaaa = [
   'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=',
 ];
 //* ************************
-// تم إضافة إعدادات لمفتاح OpenAI وإعدادات ChatGPT
-global.openai_key = 'openaiAPIKey';  // تأكد من استبدال 'YOUR_OPENAI_API_KEY' بالمفتاح الفعلي الخاص بك
-global.chatgpt = {
-  max_tokens: 150,  // عدد الكلمات الأقصى في الرد
-  temperature: 0.7,  // معدل الإبداع في الردود
-};
 
-// متابعة ملف الإعدادات
 const file = fileURLToPath(import.meta.url);
 watchFile(file, () => {
   unwatchFile(file);
