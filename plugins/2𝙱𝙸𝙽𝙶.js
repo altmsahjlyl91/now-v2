c57ac97ed4eef9e573501659e8aebc7c50b70f28
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-13 02:40:10 +0300

    Update 2𝙱𝙸𝙽𝙶.js

--- plugins/2𝙱𝙸𝙽𝙶.js
+++ plugins/2𝙱𝙸𝙽𝙶.js
@@ -1,60 +1,20 @@
 import fetch from 'node-fetch';
 import uploader from '../lib/uploadImage.js';
 
-const handler = async (m, {conn, text, command}) => {
+const handlerBK9Img = async (m, {conn, text, command}) => {
   const idioma = 'ar';
   const translations = {
     ar: {
       BK9: {
         BK9: {
-          bk9dalletext: "يرجى تقديم نص لتوليد الصورة!",
-          bk9dallewait: "جاري توليد الصورة، يرجى الانتظار...",
-          bk9dalleerr: "حدث خطأ أثناء توليد الصورة!",
-          bk9text: "يرجى تقديم نص للاستجابة!",
-          bk9err: "حدث خطأ أثناء معالجة النص!",
-          bk9imgtext: "يرجى تقديم صورة صالحة!"
+          bk9imgtext: "يرجى تقديم صورة صالحة!",
+          bk9err: "حدث خطأ أثناء معالجة الصورة!"
         }
       }
     }
   };
   const tradutor = translations[idioma].BK9.BK9;
 
-  if (command === 'تخيل2') {
-    if (!text) throw `${tradutor.bk9dalletext}`;
-
-    await conn.sendMessage(m.chat, {text: tradutor.bk9dallewait}, {quoted: m});
-
-    try {
-      const BK9 = `https://api.bk9.site/ai/photoleap?q=${encodeURIComponent(encodeURIComponent(text))}`;
-      const response = await fetch(BK9);
-      const result = await response.json();
-
-      if (result.status) {
-        await conn.sendMessage(m.chat, {image: {url: result.BK9}}, {quoted: m});
-      } else {
-        throw `${tradutor.bk9dalleerr}`;
-      }
-    } catch (error) {
-      throw `${tradutor.bk9dalleerr}`;
-    }
-  } else if (command === 'bk9') {
-    if (!text) throw `${tradutor.bk9text}`;
-
-    try {
-      conn.sendPresenceUpdate('composing', m.chat);
-      const BK9api = `https://api.bk9.site/ai/gpt4?q=${encodeURIComponent(encodeURIComponent(text))}`;
-      const BK99 = await fetch(BK9api);
-      const BK8 = await BK99.json();
-      if (BK8.status && BK8.BK9) {
-        const respuestaAPI = BK8.BK9;
-        conn.reply(m.chat, respuestaAPI, m);
-      } else {
-        throw `${tradutor.bk9err}`;
-      }
-    } catch (error) {
-      throw `${tradutor.bk9err}`;
-    }
-  } else if (command === 'حلل_صورة') {
     let BK7 = m.quoted ? m.quoted : m;
     let BK8 = (BK7.msg || BK7).mimetype || BK7.mediaType || '';
 
@@ -79,6 +39,6 @@ const handler = async (m, {conn, text, command}) => {
   }
 };
 
-handler.command = ['تخيل2', 'bk9', 'حلل_صورة'];
-handler.tags = ['ai'];
-export default handler;
+handlerBK9Img.command = ['حلل_صورة'];
+handlerBK9Img.tags = ['ai'];
+export default handlerBK9Img;
