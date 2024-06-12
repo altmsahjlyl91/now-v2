a077cc1401c03fe0abe7ac09756b07bcb15fc0c9
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-12 22:42:45 +0300

    Add files via upload

--- plugins/-1𝙱𝙰𝚁𝙳.js
+++ plugins/-1𝙱𝙰𝚁𝙳.js
@@ -1,20 +1,14 @@
 import fetch from 'node-fetch';
 
 var handler = async (m, { text, usedPrefix, command }) => {
-    if (!text && !m.quoted) throw `*يرجى وضع نص أو الرد على صورة لتحليلها.*\n\n*❏ لمزيد من الأمثلة والأوامر يرجى استخدام*\n*${usedPrefix + command} أو ${usedPrefix + command} أو ${usedPrefix + command}*`;
-    
-    let inputText = text;
-    if (m.quoted && m.quoted.mtype === 'imageMessage') {
-        inputText = await m.quoted.download();
-    }
-    
+    if (!text) throw `*يرجى وضع نص او صورة للرد عليه.*\n\n*❏ لمزيد من الأمثلة والأوامر يرجى استخدام*\n*${usedPrefix + command} أو ${usedPrefix + command} أو ${usedPrefix + command}*`;
     try {
         conn.sendPresenceUpdate('composing', m.chat);
-        var apiResponse = await fetch(`https://aemt.me/gemini?text=${encodeURIComponent(inputText)}`);
+        var apiResponse = await fetch(`https://aemt.me/gemini?text=${encodeURIComponent(text)}`);
         var res = await apiResponse.json();
         await m.reply(res.result);
     } catch (error) {
-        await conn.reply(m.chat, '*حدث خطأ أثناء المعالجة.*\n\n*يرجى التبليغ عن هذا الخطأ باستخدام*\n*#report ' + usedPrefix + command + '*\n\n' + wm, m);
+        await conn.reply(m.chat, '*حدث خطأ أثناء المعالجة.*\n\n*يرجى التبليغ عن هذا الخطأ باستخدام*\n*#report ' + usedPrefix + command + '*\n\n' + wm, fkontak, m);
         console.log(`❗❗ حدث خطأ أثناء تنفيذ ${usedPrefix + command} ❗❗`);
         console.error(error);
     }
