1612e50513e469bc2ac60d06aeddd423d3b2138c
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-14 19:44:04 +0300

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -1,11 +1,4 @@
-// TheMystic-Bot-MD@BrunoSobrino - _antitoxic.js
-
- // Para configurar o idioma, na raiz do projeto altere o arquivo config.json
-  // Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
-  // To set the language, in the root of the project, modify the config.json file.
-
-
-const toxicRegex = /مص|كسمك|rata|سخيف|imbecil|rctmre|mrd|verga|vrga|maricon/i;
+const toxicRegex = (/مص|كسمك|rata|سخيف|imbecil|rctmre|mrd|verga|vrga|maricon)/$i;
 
 export async function before(m, {isAdmin, isBotAdmin, isOwner}) {
   const datas = global
