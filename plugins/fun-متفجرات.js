f6e6b522519e9f15e5c608fe1aacf7c982284889
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-18 02:12:29 +0300

    Update fun-متفجرات.js

--- plugins/fun-متفجرات.js
+++ plugins/fun-متفجرات.js
@@ -1,23 +1,3 @@
-// الدالة الأولى: إرسال نص كصورة باستخدام واجهة برمجة التطبيقات
-let handlerTxt = async (m, { conn, text, usedPrefix, command }) => {
-    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : '';
-    if (!teks) throw `📝 ماذا أكتب؟ مثال: *${usedPrefix + command}* Hello my friend `;
-    
-    // إزالة التفاعل بسبب الخطأ
-    // m.react(rwait)
-
-    let img = global.API('fgmods', '/api/maker/txt', { text: teks }, 'apikey');
-    await conn.sendFile(m.chat, img, 'img.png', `✅ إنه أفضل مما تكتبه أنت ✍🏻`, m);
-    
-    // إزالة التفاعل بسبب الخطأ
-    // m.react(done)
-};
-
-handlerTxt.help = ['txt'];
-handlerTxt.tags = ['fun'];
-handlerTxt.command = ['اكتب'];
-
-// الدالة الثانية: لعبة المتفجرات
 let handlerBomb = async (m, { conn }) => {
     // التأكد من وجود متغير conn.bomb
     conn.bomb = conn.bomb || {};
@@ -69,4 +49,4 @@ handlerBomb.help = ["متفجرات"];
 handlerBomb.tags = ["لعبة"];
 handlerBomb.command = /^(متفجرات)$/i;
 
-export { handlerTxt, handlerBomb };
+export default handlerBomb;
