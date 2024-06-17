3486c8906e62e1b900414d68cd296651b5bd9667
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-18 02:10:26 +0300

    Update fun-اكتب.js

--- plugins/fun-اكتب.js
+++ plugins/fun-اكتب.js
@@ -1,15 +1,19 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+let handlerTxt = async (m, { conn, text, usedPrefix, command }) => {
+    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : '';
+    if (!teks) throw `📝 ماذا أكتب؟ مثال: *${usedPrefix + command}* Hello my friend `;
+    
+    // إزالة التفاعل بسبب الخطأ
+    // m.react(rwait)
 
-    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
-    if (!teks) throw `📝 ماذا أكتب؟ مثال: *${usedPrefix + command}* Hello my friend `
-    m.react(rwait)
-    let img = global.API('fgmods', '/api/maker/txt', { text: teks }, 'apikey')
-    conn.sendFile(m.chat, img, 'img.png', `✅ إنه أفضل مما تكتبه أنت ✍🏻`, m)
-    m.react(done)
+    let img = global.API('fgmods', '/api/maker/txt', { text: teks }, 'apikey');
+    await conn.sendFile(m.chat, img, 'img.png', `✅ إنه أفضل مما تكتبه أنت ✍🏻`, m);
+    
+    // إزالة التفاعل بسبب الخطأ
+    // m.react(done)
+};
 
-}
-handler.help = ['txt']
-handler.tags = ['fun']
-handler.command = ['اكتب']
+handlerTxt.help = ['txt'];
+handlerTxt.tags = ['fun'];
+handlerTxt.command = ['اكتب'];
 
-export default handler
+export default handlerTxt;
