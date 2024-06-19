395f4e16e595436fde088c722074a46b5ff86420
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-20 02:54:26 +0300

    Update -ملفات.js

--- plugins/-ملفات.js
+++ plugins/-ملفات.js
@@ -1,34 +1,26 @@
-import { tmpdir } from 'os';
-import { join } from 'path';
-import fs from 'fs';
-
+import { tmpdir } from  os 
+import { join } from  path 
+import fs from  fs 
 let handler = async (m, { args, text, usedPrefix, command }) => {
-    let info = `${usedPrefix + command} <اسم قديم> | <اسم جديد>
-📌 مثال:
+	let info = `${usedPrefix + command} <Old name> | <New name>
+*📌_ • Example:_*
 ➞ ${usedPrefix + command} inv | rpg-inv
-🗒️ ملاحظة:
-لا تستخدم امتداد .js في نهاية الاسم وتأكد من أن الكلمة لا تحتوي على مسافات "rpg-inv"`;
-    
-    if (!args[0]) throw info;
-    if (!args[1] == "|") throw `📌 مثال:
-➞ ${usedPrefix + command} inv | rpg-inv`;
-    if (!args[2]) throw `مثال:
-➞ ${usedPrefix + command} inv | rpg-inv`;
-
-    let from = args[0];
-    let to = args[2];
-    let ar = Object.keys(plugins);
-    let ar1 = ar.map(v => v.replace('.js', ''));
-    if (!ar1.includes(args[0])) return m.reply(`🗃️ غير موجود!
-==================================
-${ar1.map(v => ' ' + v).join`\n`}`);
-
-    await fs.renameSync(`./plugins/${from}.js`, `./plugins/${to}.js`);
-    conn.reply(m.chat, `تم تغيير "plugins/${from}.js" إلى "plugins/${to}.js" بنجاح`, m);
+*_🗒️ • Note:_*
+Do not use the .js extension at the end of the sentence and make sure the word does not contain spaces "rpg-inv"`
+if (!args[0]) throw info
+if (!args[1] == "|") throw `• *📌_ • Example:_*:
+➞ ${usedPrefix + command} inv | rpg-inv`
+if (!args[2]) throw `• example:
+➞ ${usedPrefix + command} inv | rpg-inv`
+let from = args[0]
+let to = args[2]
+let ar = Object.keys(plugins)
+let ar1 = ar.map(v => v.replace( .js ,   ))
+if (!ar1.includes(args[0])) return m.reply(`*🗃️ NOT FOUND!*\n==================================\n\n${ar1.map(v =>     + v).join`\n`}`)
+await fs.renameSync(`./plugins/${from}.js`, `./plugins/${to}.js`)
+conn.reply(m.chat, `Success changes "plugins/${from}.js" to "plugins/${to}.js"`, m)
 }
-
-handler.help = ['renameplugin'].map(_ => _ + " <اسم قديم> | <اسم جديد>");
-handler.command = /^(r(ملفات(file)?|f)|rn)$/i;
-handler.owner = true;
-
-export default handler;
+handler.help = [ renameplugin ].map(_=> _ + " *<old name> | <new name>*")
+handler.command = /^(r(ename(file)?|f)|rn)$/i
+handler.owner = true
+export default handler
