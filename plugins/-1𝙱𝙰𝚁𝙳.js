6433a99f1059af91a808d6556df2bdde7e7048e9
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-26 19:54:52 +0300

    Update -1𝙱𝙰𝚁𝙳.js

--- plugins/-1𝙱𝙰𝚁𝙳.js
+++ plugins/-1𝙱𝙰𝚁𝙳.js
@@ -1,42 +1,22 @@
-import _0x3ec5fb from 'node-fetch';
-import _0xdcffbe from '../lib/uploadImage.js';
-let handler = async (_0x4f9075, {
-  text: text,
-  conn: conn,
-  usedPrefix: usedPrefix,
-  command: command
-}) => {
-  if (!text && !(_0x4f9075.quoted && _0x4f9075.quoted.text)) {
-    // Fix Arabic error message
-    throw "*❆━━━═⏣⊰🦇⊱⏣═━━━❆*\n\n*🦇⤺┇ استخدام خاطء ضع رساله للرد عليها.*\n\n*❆━━━═⏣⊰🦇⊱⏣═━━━❆*";
-  }
-  try {
-    const encodedText = encodeURIComponent(text);
-    let attachment = null;
-    let mediaURL = '';
-    let quotedMessage = _0x4f9075.quoted ? _0x4f9075.quoted : _0x4f9075;
-    if ((quotedMessage.msg || quotedMessage).mimetype || quotedMessage.mediaType || '') {
-      let mimeType = (quotedMessage.msg || quotedMessage).mimetype || quotedMessage.mediaType || '';
-      if (mimeType.startsWith('video/')) {
-        return _0x4f9075.reply("*❆━━━═⏣⊰🦇⊱⏣═━━━❆*\n\n*🦇⤺┇ يرجى الرد على صورة، لا فيديو!*\n\n*❆━━━═⏣⊰🦇⊱⏣═━━━❆*");
-      }
-      attachment = await quotedMessage.download();
-      let isImage = /image\/(png|jpe?g|gif)/.test(mimeType);
-      mediaURL = await (isImage ? _0xdcffbe : _0xdcffbe)(attachment);
+import fetch from 'node-fetch';
+
+var handler = async (m, { text, usedPrefix, command }) => {
+    if (!text) throw `*يرجى وضع نص او صورة للرد عليه.*\n\n*❏ لمزيد من الأمثلة والأوامر يرجى استخدام*\n*${usedPrefix + command} أو ${usedPrefix + command} أو ${usedPrefix + command}*`;
+    try {
+        conn.sendPresenceUpdate('composing', m.chat);
+        var apiResponse = await fetch(`https://aemt.me/gemini?text=${encodeURIComponent(text)}`);
+        var res = await apiResponse.json();
+        await m.reply(res.result);
+    } catch (error) {
+        await conn.reply(m.chat, '*حدث خطأ أثناء المعالجة.*\n\n*يرجى التبليغ عن هذا الخطأ باستخدام*\n*#report ' + usedPrefix + command + '*\n\n' + wm, fkontak, m);
+        console.log(`❗❗ حدث خطأ أثناء تنفيذ ${usedPrefix + command} ❗❗`);
+        console.error(error);
     }
-    const endpointURL = mediaURL ? "https://api-darkman-3cf8c6ef66b9.herokuapp.com/googlegenai?query=" + encodedText + "&url=" + mediaURL : "https://api-darkman-3cf8c6ef66b9.herokuapp.com/googlegenai?query=" + encodedText + "&url=";
-    conn.sendPresenceUpdate("composing", text.chat);
-    const response = await _0x3ec5fb(endpointURL);
-    const result = await response.json();
-    const output = result.result;
-    _0x4f9075.reply(output);
-  } catch (error) {
-    console.error("Error:", error);
-    // Fix Arabic error message
-    throw "*❆━━━═⏣⊰🦇⊱⏣═━━━❆*\n\n*🦇⤺┇يحب شعبوط*\n\n*❆━━━═⏣⊰🦇⊱⏣═━━━❆*";
-  }
 };
-handler.help = ["bard"];
-handler.tags = ['ai'];
-handler.command = ["bard"];
+
+handler.command = ['جيمي', 'bard'];
+handler.help = ['تحليل', 'bard'];
+handler.tags = ['أدوات'];
+handler.premium = false;
+
 export default handler;
