865334dc94579fd28fa26cd4a7eb7ef5f4df7e24
altmsahjlyl91 <166942121+altmsahjlyl91@users.noreply.github.com>
2024-08-18 22:57:03 +0300

    Update dl-تطبيق.js

--- plugins/dl-تطبيق.js
+++ plugins/dl-تطبيق.js
@@ -1,18 +1,25 @@
 import {search, download} from 'aptoide-scraper';
+
 const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
- if (!text) throw `*[❗] أدخل اسم ملف APK الذي تريد البحث عنه.*`;
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.downloader_modapk
+
+
+ if (!text) throw `${tradutor.texto1}`;
   try {    
     const searchA = await search(text);
     const data5 = await download(searchA[0].id);
-    let response = `📲 *منزل Aptoide* 📲\n\n📌 *الاسم:* ${data5.name}\n📦 *الحزمة:* ${data5.package}\n🕒 *آخر تحديث:* ${data5.lastup}\n📥 *الحجم:* ${data5.size}`
+    let response = `${tradutor.texto2[0]} ${data5.name}\n${tradutor.texto2[1]}* ${data5.package}\n${tradutor.texto2[2]} ${data5.lastup}\n${tradutor.texto2[3]} ${data5.size}`
     await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
  if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-      return await conn.sendMessage(m.chat, {text: '*[ ⛔ ] الملف ثقيل جداً ولن يتم إرساله.*'}, {quoted: m});
+      return await conn.sendMessage(m.chat, {text: `${tradutor.texto3}`}, {quoted: m});
     }
     await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
   } catch {
-    throw `*[❗] خطأ، لم يتم العثور على نتائج لبحثك.*`;
+    throw `${tradutor.texto4}`;
   }    
 };
-handler.command = /^(apk|تطبيق|برنامج)$/i;
+handler.command = /^(تطبيق|modapk|dapk2|aptoide|aptoidedl)$/i;
 export default handler;
