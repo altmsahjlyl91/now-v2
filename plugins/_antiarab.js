565e845a8e55bec2e0173d3f080cb9372ea9f5fc
altmsahjlyl91 <166942121+altmsahjlyl91@users.noreply.github.com>
2024-08-18 22:41:52 +0300

    Update _antiarab.js

--- plugins/_antiarab.js
+++ plugins/_antiarab.js
@@ -19,23 +19,29 @@ handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwn
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[conn.user.jid] || {};
   if (isBotAdmin && chat.antiArab2 && !isAdmin && !isOwner && !isROwner && bot.restrict) {
-    if (m.sender.startsWith( 212  ||  212 )) {
+    if (m.sender.startsWith('212' || '212')) {
       m.reply(tradutor.texto1);
-      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender],  remove );
-      if (responseb[0].status ===  404 ) return;
+      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+      if (responseb[0].status === '404') return;
     }
 
-    if (m.sender.startsWith( 265  ||  265 )) {
+    if (m.sender.startsWith('265' || '265')) {
       m.reply(tradutor.texto2);
-      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender],  remove );
-      if (responseb[0].status ===  404 ) return;
+      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+      if (responseb[0].status === '404') return;
     }
 
-    if (m.sender.startsWith( 92  ||  92 )) {
+    if (m.sender.startsWith('92' || '92')) {
       m.reply(tradutor.texto3);
-      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender],  remove );
-      if (responseb[0].status ===  404 ) return;
+      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+      if (responseb[0].status === '404') return;
     }
+
+    if (m.sender.startsWith('234' || '234')) {
+      m.reply(tradutor.texto3);
+      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+      if (responseb[0].status === '404') return;
+    }                                                       
   }
 };
 export default handler;
