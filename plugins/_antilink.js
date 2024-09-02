e1955f102ae339d10bea80e7d6dd89826f2e4749
altmsahjlyl91 <166942121+altmsahjlyl91@users.noreply.github.com>
2024-09-03 00:52:51 +0300

    Add files via upload

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,14 +1,17 @@
 // TheMystic-Bot-MD@BrunoSobrino - _antilink.js
 
+  
 const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
-
 export async function before(m, {conn, isAdmin, isBotAdmin}) {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
+  const tradutor = _translate.plugins._antilink
+
   if (m.isBaileys && m.fromMe) {
     return !0;
   }
-  
   if (!m.isGroup) return !1;
-
   const chat = global.db.data.chats[m.chat];
   const delet = m.key.participant;
   const bang = m.key.id;
@@ -16,30 +19,19 @@ export async function before(m, {conn, isAdmin, isBotAdmin}) {
   const user = `@${m.sender.split`@`[0]}`;
   const isGroupLink = linkRegex.exec(m.text);
   const grupo = `https://chat.whatsapp.com`;
-  
-  if (isAdmin && chat.antiLink && m.text.includes(grupo)) {
-    return m.reply("🚫✨ تنبيه: نظام الحماية ضد الروابط مفعّل، لكنك مشرف في الجروب 😎. لقد تم إنقاذك! ✨🚫");
-  }
-  
+  if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(tradutor.texto1);
   if (chat.antiLink && isGroupLink && !isAdmin) {
     if (isBotAdmin) {
       const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
       if (m.text.includes(linkThisGroup)) return !0;
     }
-    await this.sendMessage(m.chat, {
-      text: "🚨「 🔗 نظام حماية الروابط 」🚨\nقمنقلع برى الجروب🏌️‍♂️ " + user + "، لقد اخترقت قواعد المجموعة، سيتم طردك فورًا! 💼🚀",
-      mentions: [m.sender]
-    }, {quoted: m});
-    
-    if (!isBotAdmin) {
-      return m.reply("⚠️ [❗معلومة❗] البوت ليس مشرفا، لذلك لا يمكنه طرد الأشخاص. ⚠️");
-    }
-    
-    await conn.sendMessage(m.chat, {
-      delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }
-    });
-    const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-    if (responseb[0].status === 404) return;
+    await this.sendMessage(m.chat, {text: tradutor.texto2, mentions: [m.sender]}, {quoted: m});
+    if (!isBotAdmin) return m.reply(tradutor.texto3);
+    if (isBotAdmin && bot.restrict) {
+      await conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
+      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+      if (responseb[0].status === '404') return;
+    } else if (!bot.restrict) return m.reply(tradutor.texto4);
   }
   return !0;
 }
--- plugins/_detectEvents.js
+++ plugins/_detectEvents.js
@@ -1,112 +1,108 @@
-// الشكر للكود إلى @Gatito-kw //
+// Creditos del codigo a @Gatito-kw //
 
 /* GitHub: https://github.com/Gatito-kw */
 
 /* Bot: https://github.com/Gatito-kw/nekobot-md */
 
-import { WAMessageStubType } from '@whiskeysockets/baileys';
+import {WAMessageStubType} from "baileys";
 import fetch from 'node-fetch';
 
-export async function قبل(m, { conn, participants }) {
+ // Para configurar o idioma, na raiz do projeto altere o arquivo config.json
+  // Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
+  // To set the language, in the root of the project, modify the config.json file.
+
+export async function before(m, {conn, participants}) {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
+  const tradutor = _translate.plugins._detectevents
+
   if (!m.messageStubType || !m.isGroup) return !0;
-  const اسم_المجموعة = (await conn.groupMetadata(m.chat)).subject;
-  const اداريي_المجموعة = participants.filter((p) => p.admin);
-  const صورة_الملف_الشخصي = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || './src/avatar_contact.png';
-  const الصورة = await (await fetch(صورة_الملف_الشخصي)).buffer();
-  const الدردشة = global.db.data.chats[m.chat];
-  const اشارة_الاعضاء = [m.sender, m.messageStubParameters[0], ...اداريي_المجموعة.map((v) => v.id)];
-  const محتوى_الاشارة = [m.sender, m.messageStubParameters[0]];
-  const رسالة_الاتصال = {
-    'key': {
-      'participants': '0@s.whatsapp.net',
-      'remoteJid': 'status@broadcast',
-      'fromMe': false,
-      'id': 'Halo'
-    },
-    'message': {
-      'contactMessage': {
-        'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-      }
-    },
-    'participant': '0@s.whatsapp.net'
-  };
+  const groupName = (await conn.groupMetadata(m.chat)).subject;
+  const groupAdmins = participants.filter((p) => p.admin);
+  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || 'https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/avatar_contact.png';
+  const img = await (await fetch(pp)).buffer();
+  const chat = global.db.data.chats[m.chat];
+  const mentionsString = [m.sender, m.messageStubParameters[0], ...groupAdmins.map((v) => v.id)];
+  const mentionsContentM = [m.sender, m.messageStubParameters[0]];
+  const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
 
-  if (الدردشة.detect2 && m.messageStubType == 29) {
-    let نص1 = `*تم ترقية عضو إلى مشرف حديثاً.*\n\n`;
-    نص1 += `*◦  المجموعة:* ${اسم_المجموعة}\n`;
-    نص1 += `*◦  المشرف الجديد:* @${m.messageStubParameters[0].split`@`[0]}\n`;
-    نص1 += `*◦  نفذ بواسطة:* @${m.sender.split`@`[0]}`;
-    await conn.sendMessage(m.chat, { image: الصورة, caption: نص1, mentions: اشارة_الاعضاء }, { quoted: رسالة_الاتصال });
+  if (chat.detect2 && m.messageStubType == 29) {
+    let txt1 = tradutor.texto1;
+    txt1 += `${tradutor.texto1_1} ${groupName}\n`;
+    txt1 += `${tradutor.tetxo1_2} @${m.messageStubParameters[0].split`@`[0]}\n`;
+    txt1 += `${tradutor.tetxo1_3} @${m.sender.split`@`[0]}`;
+    await conn.sendMessage(m.chat, {image: img, caption: txt1, mentions: mentionsString}, {quoted: fkontak2});
   }
 
-  if (الدردشة.detect2 && m.messageStubType == 30) {
-    let نص2 = `*تم تنزيل مشرف إلى عضو حديثاً.*\n\n`;
-    نص2 += `*◦  المجموعة:* ${اسم_المجموعة}\n`;
-    نص2 += `*◦  تم إزالة:* @${m.messageStubParameters[0].split`@`[0]}\n`;
-    نص2 += `*◦  نفذ بواسطة:* @${m.sender.split`@`[0]}`;
-    await conn.sendMessage(m.chat, { image: الصورة, caption: نص2, mentions: اشارة_الاعضاء }, { quoted: رسالة_الاتصال });
+  if (chat.detect2 && m.messageStubType == 30) {
+    let txt2 = tradutor.texto2;
+    txt2 += `${tradutor.texto2_1} ${groupName}\n`;
+    txt2 += `${tradutor.texto2_2} @${m.messageStubParameters[0].split`@`[0]}\n`;
+    txt2 += `${tradutor.texto2_3} @${m.sender.split`@`[0]}`;
+    await conn.sendMessage(m.chat, {image: img, caption: txt2, mentions: mentionsString}, {quoted: fkontak2});
   }
 
-  if (الدردشة.detect2 && m.messageStubType == 27) {
-    let نص3 = `*تم إضافة عضو جديد إلى المجموعة حديثاً.*\n\n`;
-    نص3 += `*◦  المجموعة:* ${اسم_المجموعة}\n`;
+  if (chat.detect2 && m.messageStubType == 27) {
+    let txt3 = tradutor.texto3;
+    txt3 += `${tradutor.texto3_1} ${groupName}\n`;
     if (!m.sender.endsWith('@g.us')) {
-      نص3 += `*◦  تم إضافة:* @${m.messageStubParameters[0].split`@`[0]}\n`;
-      نص3 += `*◦  نفذ بواسطة:* @${m.sender.split`@`[0]}`;
+      txt3 += `${tradutor.texto3_2} @${m.messageStubParameters[0].split`@`[0]}\n`;
+      txt3 += `${tradutor.texto3_3} @${m.sender.split`@`[0]}`;
     } else {
-      نص3 += `*◦  تم إضافة:* @${m.messageStubParameters[0].split`@`[0]}\n`;
+      txt3 += `${tradutor.texto3_4} @${m.messageStubParameters[0].split`@`[0]}\n`;
     }
-    await conn.sendMessage(m.chat, { image: الصورة, caption: نص3, mentions: محتوى_الاشارة }, { quoted: رسالة_الاتصال });
+    await conn.sendMessage(m.chat, {image: img, caption: txt3, mentions: mentionsContentM}, {quoted: fkontak2});
   }
 
-  if (الدردشة.detect2 && m.messageStubType == 28) {
-    let نص4 = `*تم إزالة عضو من المجموعة حديثاً.*\n\n`;
-    نص4 += `*◦  المجموعة:* ${اسم_المجموعة}\n`;
+  if (chat.detect2 && m.messageStubType == 28) {
+    let txt4 = tradutor.texto4;
+    txt4 += `${tradutor.texto4_1} ${groupName}\n`;
     if (!m.sender.endsWith('@g.us')) {
-      نص4 += `*◦  تم إزالة:* @${m.messageStubParameters[0].split`@`[0]}\n`;
-      نص4 += `*◦  نفذ بواسطة:* @${m.sender.split`@`[0]}`;
+      txt4 += `${tradutor.texto4_2} @${m.messageStubParameters[0].split`@`[0]}\n`;
+      txt4 += `${tradutor.texto4_3} @${m.sender.split`@`[0]}`;
     } else {
-      نص4 += `*◦  تم إزالة:* @${m.messageStubParameters[0].split`@`[0]}\n`;
+      txt4 += `${tradutor.texto4_4} @${m.messageStubParameters[0].split`@`[0]}\n`;
     }
-    await conn.sendMessage(m.chat, { image: { url: صورة_الملف_الشخصي }, caption: نص4, mentions: محتوى_الاشارة }, { quoted: رسالة_الاتصال });
+    await conn.sendMessage(m.chat, {image: {url: pp}, caption: txt4, mentions: mentionsContentM}, {quoted: fkontak2});
   }
 
-  if (الدردشة.detect2 && m.messageStubType == 32) {
-    let الحالة;
+  if (chat.detect2 && m.messageStubType == 32) {
+    let ax;
     if (m.messageStubParameters[0] === m.sender) {
-      الحالة = 'غادر';
+      ax = 'salido';
     } else {
-      الحالة = 'تمت إزالته';
+      ax = 'eliminado';
     }
-    let نص5 = `*تم ${الحالة} عضو من المجموعة حديثاً.*\n\n`;
-    نص5 += `*◦  المجموعة:* ${اسم_المجموعة}\n`;
-    if (الحالة === 'تمت إزالته') {
-      نص5 += `*◦  تم إزالة:* @${m.messageStubParameters[0].split`@`[0]}\n`;
-      نص5 += `*◦  نفذ بواسطة:* @${m.sender.split`@`[0]}`;
+    let txt5 = `${texto5} ${ax} ${tradutor.texto5_1}`;
+    txt5 += `${tradutor.texto5_2} ${groupName}\n`;
+    if (ax === 'eliminado') {
+      txt5 += `${tradutor.texto5_3} @${m.messageStubParameters[0].split`@`[0]}\n`;
+      txt5 += `${tradutor.texto5_4} @${m.sender.split`@`[0]}`;
     } else {
-      نص5 += `*◦  غادر:* @${m.messageStubParameters[0].split`@`[0]}\n`;
+      txt5 += `${tradutor.texto5_5} @${m.messageStubParameters[0].split`@`[0]}\n`;
     }
-    await conn.sendMessage(m.chat, { image: { url: صورة_الملف_الشخصي }, caption: نص5, mentions: محتوى_الاشارة }, { quoted: رسالة_الاتصال });
+    await conn.sendMessage(m.chat, {image: {url: pp}, caption: txt5, mentions: mentionsContentM}, {quoted: fkontak2});
   }
 
-  if (الدردشة.detect2 && m.messageStubType == 26) {
-    let الإجراء;
+  if (chat.detect2 && m.messageStubType == 26) {
+    let accion;
     if (m.messageStubParameters[0].split`@`[0] === 'on') {
-      الإجراء = 'مغلق';
+      accion = 'cerrado';
     } else {
-      الإجراء = 'مفتوح';
+      accion = 'abierto';
     }
-    let نص6 = `*تم تعديل إعدادات المجموعة حديثاً.*\n\n`;
-    نص6 += `*◦  المجموعة:* ${اسم_المجموعة}\n`;
-    نص6 += `*◦  المجموعة الآن:* ${'```' + الإجراء + '```'}\n`;
-    نص6 += `*◦  نفذ بواسطة:* @${m.sender.split`@`[0]}`;
-    await conn.sendMessage(m.chat, { image: { url: صورة_الملف_الشخصي }, caption: نص6, mentions: محتوى_الاشارة }, { quoted: رسالة_الاتصال });
+    let txt6 = tradutor.texto6;
+    txt6 += `${tradutor.texto6_1} ${groupName}\n`;
+    txt6 += `${tradutor.texto6_2} ${'```' + accion + '```'}\n`;
+    txt6 += `${tradutor.texto6_3} @${m.sender.split`@`[0]}`;
+    await conn.sendMessage(m.chat, {image: {url: pp}, caption: txt6, mentions: mentionsContentM}, {quoted: fkontak2});
   }
 
-  if (الدردشة.detect2 && m.messageStubType == 21) {
-    let نص7 = `*تم تغيير اسم المجموعة حديثاً.*\n\n`;
-    نص7 += `*◦  الاسم الجديد:* ${'```' + اسم_المجموعة + '```'}\n`;
-    نص7 += `*◦  نفذ بواسطة:* @${m.sender.split`@`[0]}`;
-    await conn.sendMessage(m.chat, { image: { url: صورة_الملف_الشخصي }, caption: نص7, mentions: محتوى_الاشارة }, { quoted: رسالة_الاتصال });
+  if (chat.detect2 && m.messageStubType == 21) {
+    let txt7 = tradutor.texto7;
+    txt7 += `${tradutor.texto7_1} ${'```' + groupName + '```'}\n`;
+    txt7 += `${tradutor.texto7_2} @${m.sender.split`@`[0]}`;
+    await conn.sendMessage(m.chat, {image: {url: pp}, caption: txt7, mentions: mentionsContentM}, {quoted: fkontak2});
   }
-} /* نهاية الدالة */
+} /* Cierre del comando */
